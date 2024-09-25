89ba020dfe629ae9a6df24ff40ed5aa7a133f183
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-09-25 11:16:37 +0200

    onboarding v1 ok

--- NewOneMore/View/StrongView/StrongListView.swift
+++ NewOneMore/View/StrongView/StrongListView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by dimitri on 17/09/2024.
 //
-
 import SwiftUI
 import SwiftData
 
@@ -14,7 +13,6 @@ struct StrongListView: View {
     @State private var showingFilterSheet = false
     @State private var selectedCategory: Categories? = nil
     @State private var searchText: String = ""
-
     
     let columns = [
         GridItem(.flexible(), spacing: 16),
@@ -24,28 +22,44 @@ struct StrongListView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                // Barre de recherche
-                HStack {
-                    TextField("Rechercher un mouvement", text: $searchText)
-                        .padding(8)
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(8)
-                        .padding(.horizontal)
+                if !strongs.isEmpty {
+                    HStack {
+                        TextField("Rechercher un mouvement", text: $searchText)
+                            .padding(8)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                    }
+                    .padding(.top, 10)
                 }
-                .padding(.top, 10)
                 
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(filteredStrongs, id: \.nom) { strong in
-                            NavigationLink(destination: StrongDetailView(strong: strong)) {
-                                CardView(colorCategory: strong.couleurCategorie, image: strong.image, nom: strong.nom, scores: strong.scores, dates: strong.dates)
+                if strongs.isEmpty {
+                    
+                    VStack(alignment: .leading) {
+                        Text("Ajoute ton premier PR")
+                            .font(.title)
+                            .fontWeight(.bold)
+                       Text("Pour selectionier ton premier mouvement, appuis sur le bouton **+** ci-dessus, tu pourra consulter les mouvements disponible pour tes **PR** et les ajouter ici. ")
+                            .foregroundStyle(.secondary)
+                    }
+                    .padding()
+                    .padding(.bottom, 140)
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 16) {
+                            ForEach(filteredStrongs, id: \.nom) { strong in
+                                NavigationLink(destination: StrongDetailView(strong: strong)) {
+                                    CardView(colorCategory: strong.couleurCategorie, image: strong.image, nom: strong.nom, scores: strong.scores, dates: strong.dates)
+                                }
                             }
                         }
+                        .padding(.all, 16)
                     }
-                    .padding(.all, 16)
                 }
-                .navigationTitle("Force")
-                .navigationBarItems(trailing: HStack {
+            }
+            .navigationTitle("Force")
+            .navigationBarItems(trailing: HStack {
+                if !strongs.isEmpty {
                     Button(action: {
                         showingFilterSheet = true
                     }) {
@@ -64,45 +78,43 @@ struct StrongListView: View {
                                 .opacity(0.8)
                         )
                     }
-                    
-                    Button(action: {
-                        showingAddItemView = true
-                    }) {
-                        ZStack {
-                            Rectangle()
-                                .fill(Color.black)
-                                .frame(width: 38, height: 38)
-                                .cornerRadius(8)
-                            Image(systemName: "plus")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                        }
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(Color.white, lineWidth: 0.6)
-                                .opacity(0.8)
-                        )
-                    }
-                })
-                .sheet(isPresented: $showingAddItemView) {
-                    AddItemView(selectedStrongs: strongs.map { $0.nom })
                 }
-                .actionSheet(isPresented: $showingFilterSheet) {
-                    ActionSheet(
-                        title: Text("Filtrer par catégorie"),
-                        buttons: categoryFilterButtons()
+                
+                
+                Button(action: {
+                    showingAddItemView = true
+                }) {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.black)
+                            .frame(width: 38, height: 38)
+                            .cornerRadius(8)
+                        Image(systemName: "plus")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.white, lineWidth: 0.6)
+                            .opacity(0.8)
                     )
                 }
+            })
+            .sheet(isPresented: $showingAddItemView) {
+                AddItemView(selectedStrongs: strongs.map { $0.nom })
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(
+                    title: Text("Filtrer par catégorie"),
+                    buttons: categoryFilterButtons()
+                )
             }
         }
     }
-
     
- 
     var filteredStrongs: [Strong] {
         let filteredByCategory: [Strong]
         
-        // Filtrer par catégorie si une catégorie est sélectionnée
         if let category = selectedCategory {
             filteredByCategory = strongs.filter { strong in
                 return strong.getCategories().contains(category)
@@ -111,7 +123,6 @@ struct StrongListView: View {
             filteredByCategory = strongs
         }
         
-        // Filtrer par texte de recherche si searchText n'est pas vide
         if searchText.isEmpty {
             return filteredByCategory
         } else {
@@ -120,15 +131,12 @@ struct StrongListView: View {
             }
         }
     }
-
-
-
-
+    
     private func categoryFilterButtons() -> [ActionSheet.Button] {
         var buttons: [ActionSheet.Button] = Categories.allCases.map { category in
-            .default(Text(category.rawValue)) {
-                selectedCategory = category
-            }
+                .default(Text(category.rawValue)) {
+                    selectedCategory = category
+                }
         }
         buttons.append(.cancel(Text("Annuler")))
         buttons.append(.default(Text("Toutes les catégories")) {
