7998411fbb92440ed79221c8c49ea9ec3d03c803
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-10-12 22:54:46 +0200

    add data wod hero

--- NewOneMore/Movement/Wod/WodMove.swift
+++ NewOneMore/Movement/Wod/WodMove.swift
@@ -73,101 +73,62 @@ let wodMoves = [
     // WOD Heroes
     WodMove(
         nom: "DT",
-        subtitle: """
-        Le WOD DT est un entraînement Hero dans le CrossFit, dédié à la mémoire du sergent de l’armée de l’air américaine Timothy P. Davis, mort en service en Afghanistan en 2009. Ce WOD est un hommage à son courage et à son sacrifice.
-
-        Le WOD DT se compose de 5 rounds avec une barre chargée, où l’athlète réalise les mouvements suivants :
-
-        • 12 deadlifts (soulevés de terre),
-        • 9 hang power cleans (épaulés depuis le haut des genoux),
-        • 6 push jerks (épaulés-jetés).
-
-        L’objectif est de compléter les 5 rounds le plus rapidement possible. DT teste principalement la force des athlètes, leur endurance musculaire et leur capacité à maintenir un effort sous fatigue. Il est souvent réalisé avec une charge standard de 70 kg (155 lb) pour les hommes et 47,5 kg (105 lb) pour les femmes. Ce WOD est très apprécié, mais aussi redouté, car il demande de la technique et de l’effort continu, avec des mouvements explosifs tout en gérant la fatigue musculaire.
-        """,
+        subtitle: NSLocalizedString("DT", comment: ""),
         imageName: "DT",
         category: .hero
     ),
     WodMove(
         nom: "Murph",
-        subtitle: """
-        Le WOD Murph est un entraînement Hero dans le CrossFit, dédié à la mémoire du lieutenant de la Navy SEAL Michael P. Murphy, tombé au combat en Afghanistan en 2005. Ce WOD honore son courage et son sacrifice.
-
-        Le WOD Murph se compose de :
-
-        • 1 mile (1,6 km) de course,
-        • 100 pull-ups,
-        • 200 push-ups,
-        • 300 air squats,
-        • 1 mile (1,6 km) de course.
-
-        Traditionnellement, ce WOD est réalisé avec un gilet lesté de 20 lb (9 kg) pour les hommes et de 14 lb (6 kg) pour les femmes. L’objectif est de compléter cet enchaînement le plus rapidement possible. Murph est un test ultime d'endurance et de force mentale, exigeant une gestion efficace de l'effort sur une longue période.
-        """,
+        subtitle: NSLocalizedString("Murph", comment: ""),
         imageName: "Murph",
         category: .hero
     ),
     WodMove(
         nom: "Wilmot",
-        subtitle: """
-        Le WOD Wilmot est un entraînement Hero dans le CrossFit, dédié à la mémoire du sergent de l’armée de l’air américaine Andrew Wilmot, tombé au combat en Afghanistan en 2010. Ce WOD honore son service et son sacrifice.
-
-        Le WOD Wilmot se compose de 6 rounds de :
-
-        • 50 air squats,
-        • 25 ring dips.
-
-        L’objectif est de compléter les 6 rounds le plus rapidement possible. Wilmot met à l’épreuve l’endurance musculaire des jambes et des bras, tout en exigeant de la constance dans l'effort. Bien qu’il ne nécessite aucun équipement lourd, ce WOD est éprouvant en raison du volume élevé de répétitions.
-        """,
+        subtitle: NSLocalizedString("Wilmot", comment: ""),
         imageName: "Wilmot",
         category: .hero
     ),
     WodMove(
         nom: "Jerry",
-        subtitle: """
-        Le WOD Jerry est un entraînement Hero dans le CrossFit, dédié à la mémoire du sergent-chef de l'armée américaine Jerry D. Patton, mort en service en 2008. Ce WOD rend hommage à son service et à son sacrifice.
-
-        Le WOD Jerry se compose de :
-
-        • 1 mile (1,6 km) de course,
-        • 2000 mètres de rameur,
-        • 1 mile (1,6 km) de course.
-
-        L’objectif est de compléter cet enchaînement le plus rapidement possible. Jerry est un test d'endurance cardiovasculaire et pulmonaire, exigeant à la fois de la vitesse et de la résistance sur de longues distances. Ce WOD ne nécessite pas de poids, mais la charge physique repose sur l'endurance, la régularité et l'effort soutenu.
-        """,
+        subtitle: NSLocalizedString("Jerry", comment: ""),
         imageName: "Jerry",
         category: .hero
     ),
     WodMove(
         nom: "Badger",
-        subtitle: """
-        Le WOD Badger est un entraînement Hero dans le CrossFit, dédié à la mémoire du sous-officier de première classe Mark Carter, surnommé "Badger", un Navy SEAL mort en service en Irak en 2007. Ce WOD honore son sacrifice et son service.
-
-        Le WOD Badger se compose de 3 rounds de :
-
-        • 30 squat cleans (42,5 kg / 95 lb),
-        • 30 pull-ups,
-        • 800 mètres de course.
-
-        L’objectif est de compléter les 3 rounds le plus rapidement possible. Badger combine force et endurance avec un travail intense de squat cleans, de pull-ups, et de course, testant à la fois la résistance musculaire et la capacité cardiovasculaire.
-        """,
+        subtitle: NSLocalizedString("Badger", comment: ""),
         imageName: "Badger",
         category: .hero
     ),
     WodMove(
         nom: "Glen",
-        subtitle: """
-        Le WOD Glen est un entraînement Hero dans le CrossFit, dédié à la mémoire du lieutenant-colonel de l'armée américaine **Glen Doherty**, tué en service lors de l'attaque de Benghazi, en Libye, en 2012. Ce WOD honore son sacrifice et son service.
-
-        Le WOD Glen se compose de :
-
-        • 30 clean and jerks (60 kg / 135 lb),
-        • 1 mile (1,6 km) de course,
-        • 10 rope climbs (4,5 mètres),
-        • 1 mile (1,6 km) de course,
-        • 100 burpees.
-
-        L’objectif est de compléter cet enchaînement le plus rapidement possible. Glen est un test d'endurance et de force, combinant des mouvements lourds et techniques avec de l'endurance cardiovasculaire, notamment à travers la course et les burpees.
-        """,
+        subtitle: NSLocalizedString("Glen", comment: ""),
         imageName: "Glen",
+        category: .hero
+    ),
+    WodMove(
+        nom: "Michael",
+        subtitle: NSLocalizedString("Michael", comment: ""),
+        imageName: "Michael",
+        category: .hero
+    ),
+    WodMove(
+        nom: "Kalsu",
+        subtitle: NSLocalizedString("Kalsu", comment: ""),
+        imageName: "Kalsu",
+        category: .hero
+    ),
+    WodMove(
+        nom: "Josh",
+        subtitle: NSLocalizedString("Josh", comment: ""),
+        imageName: "Josh",
+        category: .hero
+    ),
+    WodMove(
+        nom: "Blake",
+        subtitle: NSLocalizedString("Blake", comment: ""),
+        imageName: "Blake",
         category: .hero
     ),
     
