c3a2f9ab44f9dc38cebbfdec0415cb7b770305ba
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-10-17 10:29:38 +0200

    PRE CHANGEMENT BUILD

--- NewOneMore/Component/SplashScreenView.swift
+++ NewOneMore/Component/SplashScreenView.swift
@@ -8,46 +8,40 @@
 import SwiftUI
 
 struct SplashScreenView: View {
-    @Environment(\.colorScheme) var colorScheme // Accède au mode sombre ou clair
+    @Environment(\.colorScheme) var colorScheme
     @State private var scale: CGFloat = 0.1
     @State private var rotation: Double = 0
     @State private var showText: Bool = false
     
     var body: some View {
         VStack {
-//            Image(colorScheme == .dark ? "addMore" : "addMoreDark") // Change d'image selon le mode
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 100, height: 100) // Taille de départ de l'image
             Text("+")
             .font(Font.custom("edosz", size: 150, relativeTo: .title))
                 .scaleEffect(scale)
                 .rotationEffect(.degrees(rotation))
                 .onAppear {
                     withAnimation(.easeInOut(duration: 2.0)) {
-                        scale = 1.5 // Agrandissement de l'image
-                        rotation = 360 // Rotation de 360 degrés
+                        scale = 1.5
+                        rotation = 360
                     }
-                    
-                    // Affiche le texte après l'animation de l'image
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                         withAnimation(.easeIn) {
                             showText = true
                         }
                     }
                 }
-            
+
             if showText {
                 VStack {
-                    Text("One More")
-                        .font(Font.custom("edosz", size: 40, relativeTo: .title))
+                    Text("One-More")
+                        .font(Font.custom("edosz", size: 70, relativeTo: .title))
                         .fontWeight(.black)
                         .fontWidth(.expanded)
                         .foregroundColor(.primary)
                         .transition(.opacity)
                     Text("Slogan")
                         .font(.system(size: 11))
-                        .fontWeight(.ultraLight)
+                        .fontWeight(.regular)
                         .fontWidth(.expanded)
                         .transition(.opacity)
                 }
